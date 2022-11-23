8071ed74cd18b608a4064bd681ee9bfbb055070f
F_D <den9152@yandex.ru>
2022-11-23 19:37:02 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+// создать два делегата
+// во второй делегат сложить сложение,
+// вычитание и умножение.
+// все осуществить через лямбда выражения
+
+
+one f = () => { Console.WriteLine("helloy"); };
+two t = (int a, int b) => 
+{
+    Console.WriteLine($"{a} + {b} = {a+b}");
+};
+
+t+= (int a, int b) =>
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+};
+
+
+t(1, 2);
+
+
+
+
+
+public delegate void one();
+public delegate void two(int a, int b);
